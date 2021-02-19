b4650f9e29d6b410aa462c7606e727d1b9240649
gautamb97 <www.gautam971997@gmail.com>
2021-02-19 20:33:18 +0530

    Added:Last name validation

--- Registration.java
+++ Registration.java
@@ -3,15 +3,28 @@ import java.util.regex.Pattern;
 
 public class Registration {
 	static Scanner input = new Scanner(System.in);
-	//First name validation first name starts with Caps
+
+	// First name validation first name starts with Caps
 	public void firstNameValidation() {
 		System.out.println("Enter your First Name ");
 		String firstName = input.next();
-		if(Pattern.matches("^[A-Z][A-Za-z]{2,}$", firstName)) {
-			}
-		else {
-			System.out.println("Invalid format for First Name it should start with minimum One Caps re-enter your first name");
+		if (Pattern.matches("^[A-Z][A-Za-z]{2,}$", firstName)) {
+		} else {
+			System.out.println(
+					"Invalid format for First Name it should start with minimum One Caps re-enter your first name");
 			firstNameValidation();
+		}
+	}
+
+	// Last name validation last name starts with Caps
+	public void lastNameValidation() {
+		System.out.println("Enter your last Name ");
+		String lastName = input.next();
+		if (Pattern.matches("^[A-Z][A-Za-z]{2,}$", lastName)) {
+		} else {
+			System.out.println(
+					"Invalid format for Last Name it should start with minimum One Caps re-enter your last name");
+			lastNameValidation();
 		}
 	}
 
@@ -19,6 +32,7 @@ public class Registration {
 		System.out.println("Welcome for Registration");
 		Registration validation = new Registration();
 		validation.firstNameValidation();
+		validation.lastNameValidation();
 	}
 
 }
